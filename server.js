c5f0eb6b0ede8341f19ea8b39e925c1fc98a6d5c
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-06-28 19:36:27 -0500

    correct server.js file

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 // Server only are stactic files from the dist directory
 app.use(express.static('./dist/my-first-heroku-app'));
-app.get('/*', (req, res) => {
+app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/my-first-heroku-app'}),
-});
+);
 // Tell the app how to start on Heroku
 app.listen(process.env.PORT || 8080);
