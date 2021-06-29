e04796b6b2d5fb48b4552a387b0e341532ba53b6
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-06-28 19:23:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Server only are stactic files from the dist directory
-app.use(express.static('./codeAlong/dist/my-first-heroku-app'));
+app.use(express.static('./dist/my-first-heroku-app'));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/my-first-heroku-app/index.html));
+  res.sendFile('index.html', {root: 'dist/my-first-heroku-app'}),
 });
 // Tell the app how to start on Heroku
 app.listen(process.env.PORT || 8080);
